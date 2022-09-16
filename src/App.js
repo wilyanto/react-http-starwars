38501592056a3dd6c8d9baa6fb71a1df0f49dd10
Wilyanto <kada.indonesia@gmail.com>
2022-09-16 18:37:23 +0700

    remove dummyMovies

--- src/App.js
+++ src/App.js
@@ -23,20 +23,6 @@ function App() {
         setMovies(transformedMovies);
       });
   }
-  // const dummyMovies = [
-  //   {
-  //     id: 1,
-  //     title: 'Some Dummy Movie',
-  //     openingText: 'This is the opening text of the movie',
-  //     releaseDate: '2021-05-18',
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Some Dummy Movie 2',
-  //     openingText: 'This is the second opening text of the movie',
-  //     releaseDate: '2021-05-19',
-  //   },
-  // ];
 
   return (
     <React.Fragment>
